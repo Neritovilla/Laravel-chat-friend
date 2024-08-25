28ae37871ff321b6fe6c3dec2d4fe6b438c09a8a
Erick Villavicencio <villaeric86@gmail.com>
2024-08-25 18:35:58 -0500

    feat: basic structure proyect

---
+++ database/migrations/2024_08_25_224308_create_friends_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friends', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('from_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('to_id')->constrained('users')->onDelete('cascade');
+            $table->boolean('accepted')->default(false);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('friends');
+    }
+};
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+    <x-container>
+       <x-card class="mb-4">
+        {{ __("YOU ARE LOGONS")}}
+       </x-card>
+       <x-card>
+        {{ __("YOU ARE LOGONS 2")}}
+       </x-card>
+    </x-container>
+</x-app-layout>
